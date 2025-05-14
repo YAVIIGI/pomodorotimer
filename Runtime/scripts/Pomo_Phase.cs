b224e5fc0752efafac799a8728fe5f3bebe31a1b
YAVIIGI <viii8lull@gmail.com>
2025-05-14 12:44:54 +0900

    initial commit

---
+++ Runtime/scripts/Pomo_Phase.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Pomo_Phase : UdonSharpBehaviour
+{
+    [SerializeField]
+    GameObject[] objects;
+
+    public void SetDisplay(int num)
+    {
+        for(int i=0;i<10;i++)
+        {
+            objects[i].SetActive(i == num);
+        }
+    }
+}
